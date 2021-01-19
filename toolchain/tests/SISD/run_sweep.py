86ddcb0a118b7a1f7d0899175ad65b09a7ce0e04
Conor Fyffe <cfyffe01@qub.ac.uk>
2021-01-19 21:48:30 +0000

    Fixed a change that was made for local testing

--- toolchain/tests/SISD/run_sweep.py
+++ toolchain/tests/SISD/run_sweep.py
@@ -1,7 +1,7 @@
 # Make sure is FPE discoverable
 if __name__ == "__main__":
     import sys
-    path = __file__.split("/")
+    path = __file__.split("\\")
     levels_below_FPE = path[::-1].index("FPE") + 1
     sys.path.append("\\".join(path[:-levels_below_FPE]))
 
@@ -34,7 +34,7 @@ test_sets = [
 
 def run_sweep(path="."):
 
-    return utils.run_sweep_branch(__file__.split("/")[-2], path, test_sets)
+    return utils.run_sweep_branch(__file__.split("\\")[-2], path, test_sets)
 
 if __name__ == "__main__":
     exit( run_sweep() )
